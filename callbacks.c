c02bd5586e5c024aeda4081bf8aa72ac3dca24d0
kaisabdelaoui <58690974+kaisabdelaoui@users.noreply.github.com>
2019-12-13 12:10:29 +0100

    kais version final

--- callbacks.c
+++ callbacks.c
@@ -7,226 +7,438 @@
 #include "callbacks.h"
 #include "interface.h"
 #include "support.h"
-#include "Reponse.h"
+void affichervoiture(GtkWidget *liste);
+void afficher(GtkWidget *liste);
 
-enum 
+
+enum
 {
-	ID,
-	ETAT,
-	REC,
+	MODELE,
+	CITY,
+	PRIX, 
+	CODE,
 	COLUMNS
 };
-void afficher_rec(GtkWidget *liste);
 
 
-void afficher_rec(GtkWidget *liste)
+
+
+
+
+void affichervoiture(GtkWidget *liste)
 {
-GtkCellRenderer * renderer;
-GtkTreeViewColumn *column;
-GtkTreeIter iter;
-GtkListStore *store;
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GtkTreeIter iter;
+	GtkListStore *store;
 
-char etat[1];
-char id[2];
-char rec[100];
-store=NULL;
+	char modele[30];
+	char city[30];
+	char prix[5];
+	char code[5];
+	store=NULL;
 
-FILE *f;
+	FILE *f;
 
-store=gtk_tree_view_get_model(liste);
-if (store==NULL)
+	store=gtk_tree_view_get_model(liste);
+	if(store==NULL)
+	{
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" modele",renderer,"text",MODELE,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+		
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" city",renderer,"text",CITY,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" prix",renderer,"text",PRIX,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" code",renderer,"text",CODE,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+}
+store=gtk_list_store_new(COLUMNS,G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+f=fopen("derniere_consultation.txt","r");
+
+if(f==NULL)
 {
-	renderer=gtk_cell_renderer_text_new();					column=gtk_tree_view_column_new_with_attributes("id",renderer,"text",ID,NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW (liste), column);
 
-	renderer=gtk_cell_renderer_text_new();
-column=gtk_tree_view_column_new_with_attributes("etat",renderer,"text",ETAT,NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW (liste), column);
-
-	renderer=gtk_cell_renderer_text_new();				column=gtk_tree_view_column_new_with_attributes("rec",renderer,"text",REC,NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW (liste), column);
-
-
-store=gtk_list_store_new(COLUMNS,G_TYPE_STRING,G_TYPE_STRING,G_TYPE_STRING);
-f=fopen("reclamation.txt","r");
-if (f==NULL)
-{
-return;
+	return;
 }
 else
+{f=fopen("derniere_consultation.txt","a+");
+		while(fscanf(f,"%s %s %s %s \n",modele,city,prix,code)!=EOF)
+	{
+gtk_list_store_append(store,&iter);
+
+gtk_list_store_set(store,&iter,MODELE,modele,CITY,city,PRIX,prix,CODE,code,-1);
+	}
+	fclose(f);
+gtk_tree_view_set_model(GTK_TREE_VIEW(liste), GTK_TREE_MODEL(store));
+g_object_unref(store);
+}
+}
+void afficher(GtkWidget *liste)
 {
-f=fopen("reclamation.txt","a+");
- while(fscanf(f,"%3s%3s%50s\n",id,etat,rec)!=EOF)
-  {
-    gtk_list_store_append (store, &iter);
-    gtk_list_store_set (store,      &iter,ID,id,ETAT,etat,REC,rec, -1);
-  }
-fclose(f);
-gtk_tree_view_set_model (GTK_TREE_VIEW(liste), GTK_TREE_MODEL(store));
-g_object_unref (store);
-}
-}
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GtkTreeIter iter;
+	GtkListStore *store;
 
-}
+	char modele[30];
+	char city[30];
+	char prix[5];
+	char code[5];
+	store=NULL;
 
-void
-on_Repondre_clicked                    (GtkWidget	*objet_graphique,
-                                        gpointer         user_data)
+	FILE *f;
+
+	store=gtk_tree_view_get_model(liste);
+	if(store==NULL)
+	{
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" modele",renderer,"text",MODELE,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+		
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" city",renderer,"text",CITY,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" prix",renderer,"text",PRIX,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+
+		renderer = gtk_cell_renderer_text_new ();
+		column = gtk_tree_view_column_new_with_attributes(" code",renderer,"text",CODE,NULL);
+		gtk_tree_view_append_column (GTK_TREE_VIEW (liste), column);
+}
+store=gtk_list_store_new(COLUMNS,G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+f=fopen("voiture.txt","r");
+
+if(f==NULL)
 {
-GtkWidget *afficher;
-GtkWidget *Reponse;
-afficher=lookup_widget(objet_graphique,"afficher");
-gtk_widget_destroy(afficher);
-Reponse=lookup_widget(objet_graphique,"Reponse");
-Reponse=create_Reponse();
 
-gtk_widget_show(Reponse);
+	return;
 }
+else
+{f=fopen("voiture.txt","a+");
+		while(fscanf(f,"%s %s %s %s \n",modele,city,prix,code)!=EOF)
+	{
+gtk_list_store_append(store,&iter);
 
-
-void
-on_liste_clicked                       (GtkWidget	*objet_graphique,
-                                        gpointer         user_data)
-{
-GtkWidget *afficher;
-GtkWidget *liste;
-GtkWidget *treeview1;
-afficher=lookup_widget(objet_graphique,"afficher");
-gtk_widget_destroy(afficher);
-liste=lookup_widget(objet_graphique,"liste");
-liste=create_liste();
-
-gtk_widget_show(liste);
-treeview1=lookup_widget(liste,"treeview1");
-afficher_rec(treeview1);
+gtk_list_store_set(store,&iter,MODELE,modele,CITY,city,PRIX,prix,CODE,code,-1);
+	}
+	fclose(f);
+gtk_tree_view_set_model(GTK_TREE_VIEW(liste), GTK_TREE_MODEL(store));
+g_object_unref(store);
 }
-
-
-void
-on_retour2_clicked                     (GtkWidget	*objet_graphique,
-                                        gpointer         user_data)
-{
-GtkWidget *Reponse, *Afficher;
-
-Reponse=lookup_widget(objet_graphique,"Reponse");
-
-gtk_widget_destroy(Reponse);
-Afficher=create_Afficher();
-gtk_widget_show(Afficher);
-}
-
-
-void
-on_button4_clicked                     (GtkWidget	*objet_graphique,
-                                        gpointer         user_data)
-{
-char id[3],rec[1000];
-GtkWidget *input;
-GtkWidget *inputs;
-input=lookup_widget(objet_graphique,"entry1");
-inputs=lookup_widget(objet_graphique,"entry2");
-strcpy(id,gtk_entry_get_text(GTK_ENTRY(input)));
-strcpy(rec,gtk_entry_get_text(GTK_ENTRY(inputs)));
-Reponse(id,rec);
 }
 
 
 void
 on_retour_clicked                      (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-GtkWidget *liste, *Afficher;
+{GtkWidget *window6, *window1;
+window1=lookup_widget(objet_graphique,"window1");
 
-liste=lookup_widget(objet_graphique,"liste");
+gtk_widget_destroy(window1);
+window6=lookup_widget(objet_graphique,"window6");
+window6=create_window6();
+gtk_widget_show(window6);
 
-gtk_widget_destroy(liste);
-Afficher=create_Afficher();
-gtk_widget_show(Afficher);
+
 }
 
 
 void
-on_modifier2_clicked                   (GtkWidget	*objet_graphique,
+on_ajouter_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-GtkWidget *Afficher;
-GtkWidget *modifier2;
-Afficher=lookup_widget(objet_graphique,"Afficher");
-gtk_widget_destroy(Afficher);
-modifier2=lookup_widget(objet_graphique,"modifier2");
-modifier2=create_modifier2();
+{GtkWidget *window1;
+GtkWidget *window2;
 
-gtk_widget_show(modifier2);
+window1=lookup_widget(objet_graphique,"window1");
+gtk_widget_destroy(window1);
+window2=lookup_widget(objet_graphique,"window2");
+window2=create_window2();
+gtk_widget_show(window2);
+
+
 }
 
 
 void
-on_Supprimer3_clicked                  (GtkWidget	*objet_graphique,
+on_rechercher_clicked                  (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-GtkWidget *Afficher;
-GtkWidget *Supprimer2;
-Afficher=lookup_widget(objet_graphique,"Afficher");
-gtk_widget_destroy(Afficher);
-Supprimer2=lookup_widget(objet_graphique,"Supprimer2");
-Supprimer2=create_Supprimer2();
+{GtkWidget *window1;
+GtkWidget *window4;
 
-gtk_widget_show(Supprimer2);
+window1=lookup_widget(objet_graphique,"window1");
+gtk_widget_destroy(window1);
+window4=lookup_widget(objet_graphique,"window4");
+window4=create_window4();
+gtk_widget_show(window4);
+
+
 }
 
 
 void
-on_envoyer3_clicked                    (GtkWidget	*objet_graphique,
+on_consulter_clicked                   (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-char id[3],rec[1000];
-GtkWidget *input;
-GtkWidget *inputs;
-GtkWidget *sortie;
-input=lookup_widget(objet_graphique,"entry3");
-inputs=lookup_widget(objet_graphique,"entry4");
-strcpy(id,gtk_entry_get_text(GTK_ENTRY(input)));
-strcpy(rec,gtk_entry_get_text(GTK_ENTRY(inputs)));
-modifier(id);
+{GtkWidget *window1;
+GtkWidget *window3;
+GtkWidget *treeview1;
+
+window1=lookup_widget(objet_graphique,"window1");
+gtk_widget_destroy(window1);
+window3=lookup_widget(objet_graphique,"window3");
+window3=create_window3();
+gtk_widget_show(window3);
+treeview1=lookup_widget(window3,"treeview1");
+afficher(treeview1);
+
+}
+
+
+void
+on_ajouter2_clicked                    (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{char modele[20],city[20],prix[20];
+int x;
+x=((rand()%100)+1);
+GtkWidget *input1;
+input1=lookup_widget(objet_graphique,"entry1");
+strcpy(modele,gtk_entry_get_text(GTK_ENTRY(input1)));
+GtkWidget *input2;
+input2=lookup_widget(objet_graphique,"entry2");
+strcpy(city,gtk_entry_get_text(GTK_ENTRY(input2)));
+GtkWidget *input3;
+input3=lookup_widget(objet_graphique,"entry3");
+strcpy(prix,gtk_entry_get_text(GTK_ENTRY(input3)));
+FILE *f;
+f=fopen("voiture.txt","a"); 
+fprintf(f,"%s	   %s 	    %s	 	%d \n",modele,prix,city,x);
+fclose(f);
+
+GtkWidget *window2, *window1;
+window2=lookup_widget(objet_graphique,"window2");
+
+gtk_widget_destroy(window2);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
+}
+
+
+
+void
+on_retour1_clicked                     (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{GtkWidget *window2, *window1;
+window2=lookup_widget(objet_graphique,"window2");
+
+gtk_widget_destroy(window2);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
+
+}
+
+
+void
+on_retour3_clicked                     (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{GtkWidget *window3, *window1;
+window3=lookup_widget(objet_graphique,"window3");
+
+gtk_widget_destroy(window3);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
+
 }
 
 
 void
 on_retour4_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-GtkWidget *modifier2, *Afficher;
+{GtkWidget *window4, *window1;
+window4=lookup_widget(objet_graphique,"window4");
 
-modifier2=lookup_widget(objet_graphique,"modifier2");
+gtk_widget_destroy(window4);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
 
-gtk_widget_destroy(modifier2);
-Afficher=create_Afficher();
-gtk_widget_show(Afficher);
 }
 
 
 void
-on_supprime_clicked                    (GtkWidget	*objet_graphique,
+on_rechercher4_clicked                 (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
 {
-char id[3];
-GtkWidget *input;
-input=lookup_widget(objet_graphique,"entry5");
-strcpy(id,gtk_entry_get_text(GTK_ENTRY(input)));
-supprimer_rec(id);
+
+}
+
+
+void
+on_retour5_clicked                     (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{GtkWidget *window5, *window1;
+window5=lookup_widget(objet_graphique,"window5");
+
+gtk_widget_destroy(window5);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
+
+}
+
+
+void
+on_reserver5_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{GtkWidget *window5, *window6;
+window5=lookup_widget(objet_graphique,"window5");
+
+gtk_widget_destroy(window5);
+window6=lookup_widget(objet_graphique,"window6");
+window6=create_window6();
+gtk_widget_show(window6);
+
+
+}
+
+
+void
+on_supprimer5_clicked                  (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{int y,x,et,z;
+char modele[30], nom[20] , city[30] , prix[5];
+GtkWidget *window5;
+GtkWidget *window1;
+GtkWidget *input1;
+x=lookup_widget(objet_graphique,"entry7");
+strcpy(nom,gtk_entry_get_text(GTK_ENTRY(input1)));
+
+
+FILE *f;
+FILE *F;
+f=fopen("junk.txt","a"); 
+F=fopen("voiture.txt","r");
+fscanf(F,"%s %s %s %d\n",modele,prix,city,z);
+if((int)(x) != z)
+{fprintf(f,"%s %s %s %d\n",modele,prix,city,y);}
+fclose(f);
+fclose(F);
+f=fopen("voiture.txt","w");
+F=fopen("junk.txt","r");
+fscanf(f,"%s %s %s %d\n",nom,prix,city,y);
+while(f!=EOF)
+{fprintf(F,"%s %s %s %d\n",nom,prix,city,y);}
+
+window5=lookup_widget(objet_graphique,"window5");
+gtk_widget_destroy(window5);
+window1=lookup_widget(objet_graphique,"window1");
+window1=create_window1();
+gtk_widget_show(window1);
+
+
+}
+
+
+void
+on_modifier5_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{int y,x,et,z;
+char modele[30] ,nom[20], city[30] , prix[5];
+GtkWidget *window5;
+GtkWidget *window1;
+GtkWidget *window2;
+GtkWidget *input1;
+x=lookup_widget(objet_graphique,"entry7");
+strcpy(nom,gtk_entry_get_text(GTK_ENTRY(input1)));
+
+
+FILE *f;
+FILE *F;
+f=fopen("junk.txt","a"); 
+F=fopen("voiture.txt","r");
+fscanf(F,"%s %s %s %d\n",modele,prix,city,z);
+if((int)(x) != z)
+{fprintf(f,"%s %s %s %d\n",modele,prix,city,y);}
+fclose(f);
+fclose(F);
+f=fopen("voiture.txt","w");
+F=fopen("junk.txt","r");
+fscanf(f,"%s %s %s %d\n",nom,prix,city,y);
+while(f!=EOF)
+{fprintf(F,"%s %s %s %d\n",nom,prix,city,y);}
+
+window5=lookup_widget(objet_graphique,"window5");
+gtk_widget_destroy(window5);
+window1=lookup_widget(objet_graphique,"window1");
+window2=create_window2();
+gtk_widget_show(window2);
+
+
 }
 
 
 void
 on_retour6_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data)
-{
-GtkWidget *Supprimer2, *Afficher;
+{GtkWidget *window6, *window5;
+window6=lookup_widget(objet_graphique,"window6");
 
-Supprimer2=lookup_widget(objet_graphique,"Supprimer2");
+gtk_widget_destroy(window6);
+window5=lookup_widget(objet_graphique,"window5");
+window5=create_window5();
+gtk_widget_show(window5);
 
-gtk_widget_destroy(Supprimer2);
-Afficher=create_Afficher();
-gtk_widget_show(Afficher);
 }
+
+
+void
+on_reserver6_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data)
+{
+char nom[20],prenom[20],cin[20];
+int j1,m1,a1,j2,m2,a2;
+GtkWidget *input1;
+input1=lookup_widget(objet_graphique,"entry10");
+strcpy(nom,gtk_entry_get_text(GTK_ENTRY(input1)));
+GtkWidget *input2;
+input2=lookup_widget(objet_graphique,"entry9");
+strcpy(prenom,gtk_entry_get_text(GTK_ENTRY(input2)));
+GtkWidget *input3;
+input3=lookup_widget(objet_graphique,"entry8");
+strcpy(cin,gtk_entry_get_text(GTK_ENTRY(input3)));
+GtkWidget *input4;
+input4=lookup_widget(objet_graphique,"spinbutton4");
+j1=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input4));
+GtkWidget *input5;
+input5=lookup_widget(objet_graphique,"spinbutton5");
+m1=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input5));
+GtkWidget *input6;
+input6=lookup_widget(objet_graphique,"spinbutton6");
+a1=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input6));
+GtkWidget *input7;
+input7=lookup_widget(objet_graphique,"spinbutton1");
+j2=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input7));
+GtkWidget *input8;
+input8=lookup_widget(objet_graphique,"spinbutton2");
+m2=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input8));
+GtkWidget *input9;
+input9=lookup_widget(objet_graphique,"spinbutton3");
+a2=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(input9));
+FILE *f;
+f=fopen("reservation.txt","a"); 
+fprintf(f,"%s %s   %s : %d / %d / %d until %d / %d / %d \n",nom,prenom,cin,j1,m1,a1,j2,m2,a2);
+fclose(f);
+}
+
+
 
--- callbacks.h
+++ callbacks.h
@@ -1,46 +1,59 @@
 #include <gtk/gtk.h>
 
-
-void
-on_Repondre_clicked                    (GtkWidget	*objet_graphique,
-                                        gpointer         user_data);
-
-void
-on_liste_clicked                       (GtkWidget	*objet_graphique,
-                                        gpointer         user_data);
-
-void
-on_retour2_clicked                     (GtkWidget	*objet_graphique,
-                                        gpointer         user_data);
-
-void
-on_button4_clicked                     (GtkWidget	*objet_graphique,
-                                        gpointer         user_data);
-
 void
 on_retour_clicked                      (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
 
+
+
 void
-on_modifier2_clicked                   (GtkWidget	*objet_graphique,
+on_ajouter_clicked                     (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+
+
+void
+on_rechercher_clicked                  (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+void
+on_consulter_clicked                   (GtkWidget	*objet_graphique,
+
+                                        gpointer         user_data);
+void
+on_ajouter2_clicked                    (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+void
+on_retour1_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
 
 void
-on_Supprimer3_clicked                  (GtkWidget	*objet_graphique,
+on_retour3_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
-
-void
-on_envoyer3_clicked                    (GtkWidget	*objet_graphique,
-                                        gpointer         user_data);
-
 void
 on_retour4_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
-
 void
-on_supprime_clicked                    (GtkWidget	*objet_graphique,
+on_rechercher4_clicked                 (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
 
+void
+on_retour5_clicked                     (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_reserver5_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_supprimer5_clicked                  (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+void
+on_modifier5_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
 void
 on_retour6_clicked                     (GtkWidget	*objet_graphique,
                                         gpointer         user_data);
+
+void
+on_reserver6_clicked                   (GtkWidget	*objet_graphique,
+                                        gpointer         user_data);
+
